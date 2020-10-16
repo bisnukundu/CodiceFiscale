15786c2098391703d9d2fa832ec3243e86885dd2
bisnukundu <bisnukundu@gmail.com>
2020-10-17 00:55:56 +0600

    for me complate

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
 function bisnu(Bisnu) {
     return document.querySelector(Bisnu);
 }
+function msg_show(message) {
+    bisnu('.msg').style.display = "block";
+    bisnu('.msg').innerHTML = `${message}<button type="button" class="close" data-dismiss="alert" aria-label="Close">
+        <span aria-hidden="true">&times;</span>
+    </button>`;
+}
 var CodiceFiscale = require('codice-fiscale-js');
 const show_code = bisnu(".codice_fiscal");
 const birth_date = bisnu(".birth_date");
@@ -36,16 +42,13 @@ cal_btn.addEventListener('submit', function (e) {
     });
     show_code.innerHTML = cf.code;
     if (cf.birthplace.prov) {
-        bisnu('.code_show_place').classList.add('code_show_place_block')
-        console.log("okay")
-        bisnu('.msg').style.display="block";
-        bisnu('.msg').append('Codice Fiscale Ready');
-    } else {
-        bisnu('.msg').append('Something is wrong');
-        bisnu('.msg').style.display="block";
-        
+        bisnu('.code_show_place').classList.add('code_show_place_block');
+        msg_show('Codice Fiscale Succesfulley');
+    }else{
+        bisnu('.code_show_place').classList.remove('code_show_place_show');
+        msg_show('Something is worng');
     }
-this.reset()
+    this.reset()
 
 })
 // Making Fiscale Code End
@@ -58,6 +61,7 @@ const code_form = bisnu(".code_form");
 const data_write_btn = bisnu(".data_write_btn");
 const check_btn = bisnu(".check_btn");
 const code_show = bisnu('.fiscale_code_show');
+
 // form class select 
 const birth_place_show = bisnu('.birth_place_show');
 const birth_date_show = bisnu('.birth_date_show');
@@ -70,28 +74,36 @@ data_show_btn.addEventListener("click", function () {
 check_btn.addEventListener('submit', function (e) {
     e.preventDefault();
     //BSNBNK20P29A089A
-    var ds = new CodiceFiscale(code_show.value);
-    var birthday = ds.toJSON().birthday;
-    var birthplace = ds.toJSON().birthplace;
-    var birthplaceProvincia = ds.toJSON().birthplaceProvincia;
-    var gender = ds.toJSON().gender;
-    birth_place_show.value=`${birthplace} (${birthplaceProvincia})`;
-    birth_date_show.value=birthday;
-    if(gender == 'M'){
-        gender_show_male.checked=true
-        gender_show_female.disabled=true
-    }else if(gender == 'F'){
-        gender_show_female.checked=true
-        gender_show_male.disabled=true
+    var isValid = CodiceFiscale.check(code_show.value);
+    if (isValid) {
+        bisnu('.code_check_and_hide').classList.add('code_check_and_show')
+        var ds = new CodiceFiscale(code_show.value);
+        var birthday = ds.toJSON().birthday;
+        var birthplace = ds.toJSON().birthplace;
+        var birthplaceProvincia = ds.toJSON().birthplaceProvincia;
+        var gender = ds.toJSON().gender;
+        birth_place_show.value = `${birthplace} (${birthplaceProvincia})`;
+        birth_date_show.value = birthday;
+        if (gender == 'M') {
+            gender_show_male.checked = true
+            gender_show_female.disabled = true
+        } else if (gender == 'F') {
+            gender_show_female.checked = true
+            gender_show_male.disabled = true
+        }
+
+    } else {
+        msg_show('Invalidate Code')
+        bisnu('.code_check_and_hide').classList.remove('code_check_and_show')
     }
-    
+
 })
 
 // Copy code start 
 const btn_copy = bisnu(".btn_copy");
 const code_copy = bisnu(".code_copy");
 window.navigator.clipboard.writeText(code_copy.innerText);
-btn_copy.onclick = function(){
+btn_copy.onclick = function () {
     window.navigator.clipboard.writeText(code_copy.innerText);
     alert("Copy Successfully");
 }
